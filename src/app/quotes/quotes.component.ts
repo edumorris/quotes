0f60de2fffbb93e7118a892eba921739bb710331
edumorris <eduuwachira@gmail.com>
2020-06-18 16:31:58 +0300

    Highlight directive and functionality added. To review

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,11 +8,25 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
 
+  ind: number;  
+
   quotes: Quotes[] = [
     {id: 1, submitter: "Elly Konn", quote: "The best is yet to come", author: "Unknown", date: new Date(2020, 3, 6), upvote: 21, downvote: 2},
       {id: 2, submitter: "Lena Lorr", quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela", date: new Date(2020, 4, 12), upvote: 205, downvote: 20},
       {id: 2, submitter: "Malon Kups", quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", author: "James Cameron", date: new Date(2020, 6, 12), upvote: 110, downvote: 50}
   ]
+
+  highlightQuote(){
+    for (let i = 0; i < this.quotes.length; i++) {
+      let ind = 0;
+      
+      if(this.quotes[i].upvote < this.quotes[i++].upvote) {
+        ind = i;
+      } else{
+        ind = i++
+      }
+    }
+  }
 
   constructor() { }
 

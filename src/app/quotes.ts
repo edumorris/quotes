ed58c5000b57e4d666abd7c1a3f2f6aa9edddf67
edumorris <eduuwachira@gmail.com>
2020-06-18 15:57:28 +0300

    Quotes class created and imported to the main application

---
+++ src/app/quotes.ts
@@ -0,0 +1,9 @@
+export class Quotes {
+    id: number;
+    submitter: string;
+    quote: string;
+    author: string;
+    date: Date;
+    upvote: number;
+    downvote: number;
+}
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -7,17 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes: any[]
+  quotes: Quotes[] = [
+    {id: 1, submitter: "Elly Konn", quote: "The best is yet to come", author: "Unknown", date: new Date(2020, 3, 6), upvote: 21, downvote: 2},
+      {id: 2, submitter: "Lena Lorr", quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela", date: new Date(2020, 4, 12), upvote: 205, downvote: 20},
+      {id: 2, submitter: "Malon Kups", quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", author: "James Cameron", date: new Date(2020, 6, 12), upvote: 110, downvote: 50}
+  ]
 
-  constructor() {
-    this.quotes = [
-      {id: 1, submitter: "Elly Konn", quote: "The best is yet to come", author: "Unknown", Date: new Date(2020, 3, 6), upvote: 21, downvote: 2},
-      {id: 2, submitter: "Lena Lorr", quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.", author: "Nelson Mandela", Date: new Date(2020, 4, 12), upvote: 205, downvote: 20},
-      {id: 2, submitter: "Malon Kups", quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", author: "James Cameron", Date: new Date(2020, 6, 12), upvote: 110, downvote: 50}
-    ]
-   }
+  constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
